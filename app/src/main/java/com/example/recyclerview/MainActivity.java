efa00348de097d1d134de0a3de20c3ca648dd6e1
Arvid2176 <2550889007@qq.com>
2017-04-26 10:38:10 +0800

    recyclerView 瀑布排列 加点击事件

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.recyclerview;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
 import com.example.recyclerview.entity.Fruit;
 import com.example.recyclerview.util.FruitAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private List<Fruit>  fruitList=new ArrayList<>();
@@ -21,9 +24,10 @@ public class MainActivity extends AppCompatActivity {
 
         initFruitList();
         RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recycle_view_list);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        //网格布局
+//        GridLayoutManager gridLayoutManager= new GridLayoutManager(this,3);
+        StaggeredGridLayoutManager layoutManager=new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
         FruitAdapter fruitAdapter=new FruitAdapter(fruitList);
         recyclerView.setAdapter(fruitAdapter);
 
@@ -36,40 +40,49 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<2;i++){
             Fruit apple=new Fruit();
             apple.setImgID(R.drawable.apple);
-            apple.setName("apple");
+            apple.setName(getRandomlengthName("apple"));
             fruitList.add(apple);
             Fruit banana=new Fruit();
-            banana.setName("banana");
+            banana.setName(getRandomlengthName("banana"));
             banana.setImgID(R.drawable.banana);
             fruitList.add(banana);
             Fruit orange =new Fruit();
             orange.setImgID(R.drawable.orange);
-            orange.setName("orange");
+            orange.setName(getRandomlengthName("orange"));
             fruitList.add(orange);
             Fruit watermelon=new Fruit();
-            watermelon.setName("watermelon");
+            watermelon.setName(getRandomlengthName("watermelon"));
             watermelon.setImgID(R.drawable.watermelon);
             fruitList.add(watermelon);
             Fruit pear=new Fruit();
             pear.setImgID(R.drawable.pear);
-            pear.setName("pear");
+            pear.setName(getRandomlengthName("pear"));
             fruitList.add(pear);
             Fruit grape= new Fruit();
-            grape.setName("grape");
+            grape.setName(getRandomlengthName("grape"));
             grape.setImgID(R.drawable.grape);
             fruitList.add(grape);
             Fruit pineapple =new Fruit();
             pineapple.setImgID(R.drawable.pineapple);
-            pineapple.setName("pineapple");
+            pineapple.setName(getRandomlengthName("pineapple"));
             fruitList.add(pineapple);
             Fruit strawberry=new Fruit();
-            strawberry.setName("strawberry");
+            strawberry.setName(getRandomlengthName("strawberry"));
             strawberry.setImgID(R.drawable.strawberry);
             fruitList.add(strawberry);
             Fruit kiwifruit=new Fruit();
             kiwifruit.setImgID(R.drawable.kiwifruit);
-            kiwifruit.setName("kiwifruit");
+            kiwifruit.setName(getRandomlengthName("kiwifruit"));
             fruitList.add(kiwifruit);
         }
+    }
+    private String getRandomlengthName(String name){
+        Random random=new Random();
+        int length=random.nextInt(20)+1;
+        StringBuilder stringBuilder=new StringBuilder();
+        for(int i=0;i<=length;i++){
+            stringBuilder.append(name);
+        }
+        return  stringBuilder.toString();
     }
 }
--- app/src/main/java/com/example/recyclerview/util/FruitAdapter.java
+++ app/src/main/java/com/example/recyclerview/util/FruitAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.recyclerview.R;
 import com.example.recyclerview.entity.Fruit;
@@ -22,10 +23,12 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
     private static final String TAG = "FruitAdapter";
     private List<Fruit> fruitList=new ArrayList<>();
     static class ViewHolder extends RecyclerView.ViewHolder{
+        View fruitView;
         ImageView imageView;
         TextView textView;
         public ViewHolder(View view){
             super(view);
+            fruitView=view;
             imageView=(ImageView) view.findViewById(R.id.fruit_img);
             textView=(TextView)view.findViewById(R.id.fruit_name);
         }
@@ -40,7 +43,22 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item,parent,false);
-        ViewHolder viewHolder=new ViewHolder(view);
+        final ViewHolder viewHolder=new ViewHolder(view);
+        viewHolder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fruit fruit=fruitList.get(viewHolder.getAdapterPosition());
+                Toast.makeText(v.getContext(),"you clicked view "+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        viewHolder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fruit fruit=fruitList.get(viewHolder.getAdapterPosition());
+                Toast.makeText(v.getContext(),"you clicked name"+fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Log.d(TAG, "onCreateViewHolder: "+view.toString());
         return viewHolder;
     }
